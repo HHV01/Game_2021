cc9111719bb17e6f05620dcef3bb97794caf69ab
HHV01 <hoanghongvan2001@gmail.com>
2021-12-07 00:42:09 +0700

    add change

---
+++ 2D Tutorial/Assets/Scripts/Enemy/ArrowTrap.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ArrowTrap : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] arrows;
+    private float coolDownTimer;
+
+    private void Attack()
+    {
+        coolDownTimer = 0;
+        arrows[FindArrow()].transform.position = firePoint.position;
+        arrows[FindArrow()].GetComponent<EnemyProjectile>().ActivateProjectile();
+    }
+
+    private int FindArrow()
+    {
+        for(int i= 0; i < arrows.Length; i++)
+        {
+            if (!arrows[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+    private void Update()
+    {
+        coolDownTimer += Time.deltaTime;
+        if (coolDownTimer >= attackCooldown)
+            Attack();
+    }
+}
